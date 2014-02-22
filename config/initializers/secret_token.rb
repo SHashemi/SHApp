bf1d09c62ad006ede49d68a6679eddbb5c8f114e
Sara Hashemi <sara.hashemi@ut.ac.ir>
2014-02-22 18:10:09 +0330

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SHApp::Application.config.secret_key_base = '6b99c9fff9b5d763723df40f3b6b8ecc84903a30861419523c595da89c4e8a36d05b9c5953f9c6db7a0955a2c0c97a7b6394e65acf9921bc5467716d985ed0d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SHApp::Application.config.session_store :cookie_store, key: '_SHApp_session'
